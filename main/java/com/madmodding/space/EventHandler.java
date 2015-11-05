5f366e61d187b1f24e47a3660533a9d791183ed6
MadModding <madhattergamingforlife@gmail.com>
2015-11-05 18:41:19 +0100

    EventHandler

---
+++ main/java/com/madmodding/space/EventHandler.java
@@ -0,0 +1,10 @@
+package com.madmodding.space;
+
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class EventHandler {
+	//@SubscribeEvent
+	//public static void onLivingUpdate(LivingUpdateEvent event){
+	//}
+}
