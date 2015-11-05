4bf20ffc90099744ef03ff2e64ee31fd4119fd27
MadModding <madhattergamingforlife@gmail.com>
2015-11-05 19:19:27 +0100

    Render items

---
+++ main/java/com/madmodding/space/client/render/items/ItemRenderRegister.java
@@ -0,0 +1,53 @@
+package com.madmodding.space.client.render.items;
+
+import com.madmodding.space.Main;
+import com.madmodding.space.items.ItemFragment;
+import com.madmodding.space.items.ModItems;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
+public final class ItemRenderRegister {
+
+	public static void registerItemRenderer() {
+
+		reg(ModItems.evilstaff);
+		for (int i = 0; i < ItemFragment.names.length*4; i++)
+			reg(ModItems.fragment, i);
+	}
+
+	// ==========================================================================
+
+	public static String modid = Main.MODID;
+
+	public static void reg(Item item, int i, String nameadd) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, i,
+				new ModelResourceLocation(modid + ":" + nameadd, "inventory"));
+	}
+
+	public static void reg(Item item, int i) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, i,
+				new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
+
+	public static void reg(Item item) {
+		int i = 0;
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, i,
+				new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
+
+	public static void reg(Item item, String nameext, int i) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, i,
+				new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5) + nameext, "inventory"));
+		// System.out.println(item.getUnlocalizedName().substring(5)+"_" +
+		// nameext);
+	}
+
+	public static void reg(Block block) {
+		int i = 0;
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), i,
+				new ModelResourceLocation(modid + ":" + block.getUnlocalizedName().substring(5), "inventory"));
+	}
+}
