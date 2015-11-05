6da63bfb1f1eb6c31ef2492c078f2d3d8cbd6c2b
MadModding <madhattergamingforlife@gmail.com>
2015-11-05 18:43:08 +0100

    Create AlienEgg.java

---
+++ main/java/com/madmodding/space/blocks/AlienEgg.java
@@ -0,0 +1,34 @@
+package com.madmodding.space.blocks;
+
+import com.madmodding.space.Main;
+import com.madmodding.space.blocks.tile.TileEntityAlienEgg;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class AlienEgg extends BlockContainer {
+	
+	public AlienEgg(Material mat) { 
+		super(mat);
+		this.setCreativeTab(Main.aliensTab);
+	}
+	
+	public int getRenderType() {
+		return -1;
+	}
+
+    public boolean isOpaqueCube() {
+    	return false;
+    }
+    
+    public boolean renderAsNormalBlock() {
+    	return false;
+    }
+    
+    @Override
+    public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
+    	return new TileEntityAlienEgg();
+    }
+}
