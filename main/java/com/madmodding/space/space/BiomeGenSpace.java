99a0cb02309bd8c53d973cc43f559644c1948c09
MadModding <madhattergamingforlife@gmail.com>
2015-11-05 19:23:57 +0100

    Space Biome

---
+++ main/java/com/madmodding/space/space/BiomeGenSpace.java
@@ -0,0 +1,35 @@
+package com.madmodding.space.space;
+
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BiomeGenSpace extends BiomeGenBase
+{
+    private static final String __OBFID = "CL_00000187";
+
+    public BiomeGenSpace(int p_i1990_1_)
+    {
+        super(p_i1990_1_);
+        this.spawnableMonsterList.clear();
+        this.spawnableCreatureList.clear();
+        this.spawnableWaterCreatureList.clear();
+        this.spawnableCaveCreatureList.clear();
+        this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntitySpider.class, 10, 4, 4));
+        this.topBlock = Blocks.end_stone.getDefaultState();
+        this.fillerBlock = Blocks.end_stone.getDefaultState();
+        this.theBiomeDecorator = new BiomeSpaceDecorator();
+    }
+
+    /**
+     * takes temperature, returns color
+     */
+    @SideOnly(Side.CLIENT)
+    public int getSkyColorByTemp(float p_76731_1_)
+    {
+        return 0;
+    }
+}
