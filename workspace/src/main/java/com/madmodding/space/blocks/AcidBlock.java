8239d0ea6d8a1b742924b94ecb606d42a7f59bf2
Harpreet Singh <hsgill97@gmail.com>
2015-11-10 15:04:15 -0800

    Acid Fix
    Bedrock now survives acid.
    Acid now kills entities

--- workspace/src/main/java/com/madmodding/space/blocks/AcidBlock.java
+++ workspace/src/main/java/com/madmodding/space/blocks/AcidBlock.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 import com.madmodding.space.Main;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
-import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.BlockFluidFinite;
 import net.minecraftforge.fluids.Fluid;
 
@@ -21,17 +23,24 @@ public class AcidBlock extends BlockFluidFinite {
 		setUnlocalizedName(fluidName);
 		setCreativeTab(Main.aliensTabBlock);
 	}
-    /**
+    
+	@Override
+	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+
+				DamageSource burn=new DamageSource(fluidName);
+				entityIn.attackEntityFrom(burn, 1.5F);
+			    
+		}
+	/*
      * Called randomly when setTickRandomly is set to true (used by e.g. crops to grow, etc.)
      */
 	@Override
     public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random random)
     {
 		IBlockState iblockstate = worldIn.getBlockState(pos.down());
-        Material material = iblockstate.getBlock().getMaterial();
-        if (!(material == ModFluids.acidMat))
+        if ((iblockstate.getBlock().getBlockHardness(worldIn, pos.down())>0F))
         {
-    		System.out.println("AcidFluid Debug Block Destroyed x:"+pos.getX()+" "+" y:"+(pos.getY()-1)+" z:"+pos.getZ());
+    		System.out.println(" AcidFluid Debug Block Destroyed "+iblockstate.getBlock().getUnlocalizedName()+" x:"+pos.getX()+" "+" y:"+(pos.getY()-1)+" z:"+pos.getZ());
             worldIn.setBlockToAir(pos.down());
         }    
 		
